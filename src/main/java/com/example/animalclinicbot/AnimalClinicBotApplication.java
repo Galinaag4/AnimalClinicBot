ca11537a0976469f93b0011ac51883f53ce69c77
Galinaag4 <Galinaag@yandex.ru>
2023-03-22 16:11:35 +0300

    first commit

---
+++ src/main/java/com/example/animalclinicbot/AnimalClinicBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalclinicbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalClinicBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalClinicBotApplication.class, args);
+    }
+
+}
