ec92eaa7bc19a43e40d200965b7a164999e85bc3
Scherbakova <nastyasch0806@gmail.com>
2023-03-26 22:46:30 +0300

    Revert "прописала в properties подключение к БД, добавила библиотеки в pom.xml"
    This reverts commit fbf421d5b94ccf76474012659e06306c45c61bd2.

--- src/main/java/com/example/animalclinicbot/model/Cat.java
+++ src/main/java/com/example/animalclinicbot/model/Cat.java
@@ -1,16 +1,13 @@
 package com.example.animalclinicbot.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
-//@Entity
+@Entity
 public class Cat {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     private Long id;
 
     private String nameCat;
